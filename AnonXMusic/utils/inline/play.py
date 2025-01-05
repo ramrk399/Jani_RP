6b57887b54363036d4ed8facb1d593869fe4508a
ISTKHAR-king <istkharalam225@gmail.com>
2025-01-05 08:02:24 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -33,44 +33,38 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "⚪━━━━━━━━━━"
+        bar = "◉—————————"
     elif 10 < umm < 20:
-        bar = "━⚪━━━━━━━━━"
+        bar = "—◉————————"
     elif 20 <= umm < 30:
-        bar = "🔥𝙏𝙝𝙪𝙣𝙙𝙚𝙧 𝗣𝗹𝗮𝘆𝗶𝗻𝗴"
+        bar = "——◉———————"
     elif 30 <= umm < 40:
-        bar = "━━━━⚪━━━━━━"
+        bar = "———◉——————"
     elif 40 <= umm < 50:
-        bar = "━━━━━⚪━━━━━"
+        bar = "————◉—————"
     elif 50 <= umm < 60:
-        bar = "━━━━━━━⚪━━━"
+        bar = "—————◉————"
     elif 60 <= umm < 70:
-        bar = "𝙄 𝘼𝙢 𝙇𝙞𝙫𝙚 𝙉𝙤𝙬🎧"
+        bar = "——————◉———"
     elif 70 <= umm < 80:
-        bar = "━━━━━━━━━⚪━"
+        bar = "———————◉——"
     elif 80 <= umm < 95:
-        bar = "━━━━━━━━━⚪━"
+        bar = "————————◉—"
     else:
-        bar = "━━━━━━━━━━⚪"
+        bar = "—————————◉"
     buttons = [
+        [
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
         [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
             )
-        ],
-        [
-            InlineKeyboardButton(
-                text="❰𝙊𝙬𝙣𝙚𝙧❱", url="https://t.me/ll_SEHZADA_143"
-            ),
-            InlineKeyboardButton(
-                text="❰𝗔𝗹𝗹 𝗕𝗼𝘁❱", url="https://t.me/THUNDERDEVS"
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="❰𝗣𝗥𝗢𝗠𝗢𝗧𝗜𝗢𝗡 𝗔𝗩𝗔𝗜𝗟𝗔𝗕𝗟𝗘❱", url="https://t.me/CRAZY_OP_02/51"
-            ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
@@ -85,16 +79,6 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [
-            InlineKeyboardButton(
-                text="🔥𝙏𝙝𝙪𝙣𝙙𝙚𝙧 𝗣𝗹𝗮𝘆𝗶𝗻𝗴🚩", url="https://t.me/ll_SEHZADA_143",
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="𝙄 𝘼𝙢 𝙇𝙞𝙫𝙚 𝙉𝙤𝙬🎧", url="https://t.me/THUNDERDEVS",
-            ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
